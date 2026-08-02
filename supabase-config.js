bbaa45e8af330a4cccfd491676c67f20d2583b9c
hlm-info <elhadjbouraima@yahoo.fr>
2026-08-02 03:17:34 +0200

    Add files via upload
    Ajout des fichiers du site HLM INFO PUB

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// ============================================================
+// Colle ici les identifiants de TON projet Supabase.
+// Supabase > Project Settings > API
+// ============================================================
+const SUPABASE_URL = "https://mnfsjcmmlmaoqufduzoc.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZnNqY21tbG1hb3F1ZmR1em9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzNjA0ODYsImV4cCI6MjEwMDkzNjQ4Nn0.yf5PY_VHbCRp55kU_KlMOAYxrrnGwjj5oASbC7tBz8Q";
